68cb483cc86f02466b311182ae22dadce79d8c1d
ZodicSlanser <zodic.slanser68@yahoo.com>
2023-05-06 02:01:15 +0300

    added a new grading system, added the ability to drop out of course, and enroll in one (might be moved to the controller later IDK), reformatted code for better readability

--- app/Models/Enrollment.php
+++ app/Models/Enrollment.php
@@ -10,18 +10,16 @@ class Enrollment extends Model
 {
     use HasFactory;
 
-
+    const GRADE_A = 90;
+    const GRADE_B = 80;
+    const GRADE_C = 70;
+    const GRADE_D = 60;
 
     protected $fillable = [
         'student_id',
         'course_id',
         'grade'
     ];
-    private mixed $grade;
-    const GRADE_A = 90;
-    const GRADE_B = 80;
-    const GRADE_C = 70;
-    const GRADE_D = 60;
 
     protected static function newFactory()
     {
@@ -38,20 +36,76 @@ class Enrollment extends Model
         return $this->belongsTo(Course::class, 'course_id');
     }
 
+    public function enroll(User $student, Course $course): bool
+    {
+//if student is enrolled in the course then return false
+        if ($this->is_enrolled($student, $course)) return false;
+
+//if this course has no prerequisites then enroll them
+        if (!$course->prerequisite) {
+            $this->student_id = $student->id;
+            $this->course_id = $course->id;
+            $this->save();
+            return true;
+        }
+//if this course has prerequisites and student is not enrolled in the course then check if he has passed all the prerequisites
+        if ($course->prerequisite && $this->hasPassedPrerequisite($student, $course->prerequisite)) {
+            $this->student_id = $student->id;
+            $this->course_id = $course->id;
+            $this->save();
+            return true;
+        }
+
+        return false;
+    }
+
+    public function is_enrolled(User $student, Course $course): bool
+    {
+        return $this->student_id === $student->id && $this->course_id === $course->id;
+    }
+
+    public function hasPassedPrerequisite(User $student, Course $prerequisite): bool
+    {
+        $enrollment = $this->where('student_id', $student->id)
+            ->where('course_id', $prerequisite->id)
+            ->first();
+
+        if (!$enrollment || !$this->has_passed($enrollment)) {
+            return false;
+        }
+
+        return true;
+    }
+
+    public function has_passed(Enrollment $enrollment): bool
+    {
+        return $enrollment->grade >= self::GRADE_D;
+    }
+
     public function letter_grade(): string
     {
         $numeric_grade = $this->grade;
 
-        if ($numeric_grade >= self::GRADE_A) {
-            return 'A';
-        } elseif ($numeric_grade >= self::GRADE_B) {
-            return 'B';
-        } elseif ($numeric_grade >= self::GRADE_C) {
-            return 'C';
-        } elseif ($numeric_grade >= self::GRADE_D) {
-            return 'D';
-        } else {
-            return 'F';
-        }
+        if ($numeric_grade >= self::GRADE_A) return 'A';
+        elseif ($numeric_grade >= self::GRADE_B) return 'B';
+        elseif ($numeric_grade >= self::GRADE_C) return 'C';
+        elseif ($numeric_grade >= self::GRADE_D) return 'D';
+        return 'F';
+
+    }
+
+    public function drop(User $student, Course $course): bool
+    {
+        if (!$this->is_enrolled($student, $course)) return false; // student is not enrolled in the course
+        $this->delete();
+        return true;
+    }
+
+    public function update_grade(User $student, Course $course, int $new_grade): bool
+    {
+        if (!$this->is_enrolled($student, $course)) return false; // student is not enrolled in the course
+        $this->grade = $new_grade;
+        $this->save();
+        return true;
     }
 }
