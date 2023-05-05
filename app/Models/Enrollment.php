2aca08c9c25bc6b9d2e8a2edb92569f0760f04ed
ZodicSlanser <zodic.slanser68@yahoo.com>
2023-05-06 00:43:33 +0300

    Added the letter_grade() function for possible feature use

--- app/Models/Enrollment.php
+++ app/Models/Enrollment.php
@@ -10,11 +10,18 @@ class Enrollment extends Model
 {
     use HasFactory;
 
+
+
     protected $fillable = [
         'student_id',
         'course_id',
         'grade'
     ];
+    private mixed $grade;
+    const GRADE_A = 90;
+    const GRADE_B = 80;
+    const GRADE_C = 70;
+    const GRADE_D = 60;
 
     protected static function newFactory()
     {
@@ -29,5 +36,22 @@ class Enrollment extends Model
     public function course()
     {
         return $this->belongsTo(Course::class, 'course_id');
+    }
+
+    public function letter_grade(): string
+    {
+        $numeric_grade = $this->grade;
+
+        if ($numeric_grade >= self::GRADE_A) {
+            return 'A';
+        } elseif ($numeric_grade >= self::GRADE_B) {
+            return 'B';
+        } elseif ($numeric_grade >= self::GRADE_C) {
+            return 'C';
+        } elseif ($numeric_grade >= self::GRADE_D) {
+            return 'D';
+        } else {
+            return 'F';
+        }
     }
 }
