ef8069981ebdf4eb9621f9aa3cacdfdf3ec702b7
ZodicSlanser <zodic.slanser68@yahoo.com>
2023-05-10 15:04:22 +0300

    added the routes for each page

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,17 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProfessorsController;
 
+
 Route::get('/', function () {
-    return view('welcome');
+    return view('login');
 });
+
+Route::get('/login', [UserController::class, 'index']);
+Route::post('/login/checklogin', [UserController::class, 'checklogin']);
+Route::get('/home', [UserController::class, 'successlogin']);
+Route::get('/logout', [UserController::class, 'logout']);
 
 Route::get('/professorCourses',[ProfessorsController::class, 'getCoursesByProfessorId']);
