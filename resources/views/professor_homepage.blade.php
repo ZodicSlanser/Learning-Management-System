e1b361d92aec8cc21f2dd0773ea148de204a9b9b
ZodicSlanser <zodic.slanser68@yahoo.com>
2023-05-10 18:25:05 +0300

    fixed merge errors

---
+++ resources/views/professor_homepage.blade.php
@@ -0,0 +1,44 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <title>home</title>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css"/>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <style type="text/css">
+        .box {
+            width: 600px;
+            margin: 0 auto;
+            border: 1px solid #ccc;
+        }
+    </style>
+</head>
+<body>
+<br/>
+<div class="container box">
+    @if(isset(Auth::user()->email))
+        <h3 align="center"><strong>This is professor homepage</strong></h3><br/>
+
+        <div class="alert alert-danger success-block">
+
+            <strong>Welcome {{ Auth::user()->email }}</strong>
+            <strong>Your role is {{ Auth::user()->role }}</strong>
+            <br/>
+            <a href="{{ url('/logout') }}">Logout</a>
+                <?php
+                    $courses = Auth::user()->courses;
+                    foreach($courses as $course){
+                        echo "<br/>";
+                        echo "<a href='/course/".$course->id."'>".$course->name."</a>";
+                    }
+                    ?>
+        </div>
+    @else
+        <script>window.location = "/home";</script>
+    @endif
+    <br/>
+</div>
+</body>
+
+</html>
