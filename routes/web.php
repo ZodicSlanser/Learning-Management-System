347820b599b00022566cd86708667119920d87df
ZodicSlanser <zodic.slanser68@yahoo.com>
2023-05-17 04:36:54 +0300

    added missing routes

--- routes/web.php
+++ routes/web.php
@@ -15,3 +15,18 @@ Route::get('/home', [UserController::class, 'successlogin']);
 Route::get('/logout', [UserController::class, 'logout']);
 
 Route::get('/professorCourses', [ProfessorsController::class, 'getCoursesByProfessorId']);
+
+Route::resource('/courses', CoursesController::class);
+Route::resource('/departments', DepartmentsController::class);
+Route::resource('/users', UsersController::class);
+Route::resource('/generate', GenerateController::class);
+
+Route::get('department-restore/{id}', [DepartmentsController::class, 'delete'])->name('department.delete');
+
+Route::get('course-restore/{id}', [CoursesController::class, 'delete'])->name('course.delete');
+
+Route::get('user/restore/{id}', [UsersController::class, 'restore'])->name('user.restore');
+
+Route::get('delete-enrollment', [GenerateController::class, 'deleteEnrollment'])->name('enrollment.delete');
+Route::get('/generate/download/{course_id}', [GenerateController::class, 'download'])->name('generate.download');
+
