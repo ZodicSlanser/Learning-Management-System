92cb798e7668209331fbb9194936ed16944c1d68
ZodicSlanser <zodic.slanser68@yahoo.com>
2023-05-17 04:20:05 +0300

    added the ID option to the enrollments table

--- database/migrations/2023_05_11_013724_enrollement__table_migration.php
+++ database/migrations/2023_05_11_013724_enrollement__table_migration.php
@@ -4,14 +4,14 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
     public function up()
     {
         Schema::create('enrollments', function (Blueprint $table) {
+            $table->id();
             $table->foreignId('student_id')->constrained('users');
             $table->foreignId('course_id')->constrained('courses');
             $table->integer('grade')->nullable();
