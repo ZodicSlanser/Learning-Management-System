f5c55b8a1c16ba3559ba10fcb368d53d5139dc83
ZodicSlanser <zodic.slanser68@yahoo.com>
2023-05-17 04:22:53 +0300

    added restore function ##needs to be implemented later for all views

--- app/Http/Controllers/DepartmentsController.php
+++ app/Http/Controllers/DepartmentsController.php
@@ -5,8 +5,6 @@ namespace App\Http\Controllers;
 use App\Models\Course;
 use App\Models\Department;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
-use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Redirect;
 
 class DepartmentsController extends Controller
@@ -18,8 +16,26 @@ class DepartmentsController extends Controller
     {
         //
         $departments = Department::paginate(9);
-        
-        return view('departments.index',['departments'=>$departments]);
+
+        return view('departments.index', ['departments' => $departments]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+        $formFields = $request->validate([
+
+            'name' => 'required',
+            'code' => 'required',
+
+        ]);
+
+        Department::create($formFields);
+
+        return Redirect::route('departments.index')->with('status', 'Created Successfully');
     }
 
     /**
@@ -33,31 +49,12 @@ class DepartmentsController extends Controller
     }
 
     /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        //
-        $formFields = $request -> validate([
-      
-            'name'=>'required',
-            'code'=>'required',
-       
-    ]);
- 
-   Department::create($formFields);
-
-   return Redirect::route('departments.index')->with('status','Created Succsessfult .');
-    }
-
-    /**
      * Display the specified resource.
      */
     public function show(Department $department)
     {
-        //
-       
-        return view( 'departments.show' , [ 'departments' => $department]);
+
+        return view('departments.show', ['departments' => $department]);
 
     }
 
@@ -66,8 +63,7 @@ class DepartmentsController extends Controller
      */
     public function edit(Department $department)
     {
-        //
-        return view('departments.edit', [ 'departments' => $department]);
+        return view('departments.edit', ['departments' => $department]);
 
     }
 
@@ -77,17 +73,17 @@ class DepartmentsController extends Controller
     public function update(Request $request, Department $department)
     {
         //
-        $formFields = $request -> validate([
-      
-            'name'=>'required',
-            'code'=>'required',
-       
-    ]);
+        $formFields = $request->validate([
 
-    $department->update($formFields);
+            'name' => 'required',
+            'code' => 'required',
 
-    return Redirect::route('departments.show', $department->id)->with('status','Update Successfuly .');
-   
+        ]);
+
+        $department->update($formFields);
+
+        return Redirect::route('departments.show', $department->id)->with('status', 'Updated Successfully');
+
     }
 
     /**
@@ -95,17 +91,23 @@ class DepartmentsController extends Controller
      */
     public function destroy(Department $department)
     {
-        //
-       $check = DB::table('courses')->where('department_id',$department->id)->first(); 
-       $check2 = DB::table('users')->where('department_id',$department->id)->first(); 
+        $courses = Course::where('department_id', '=', $department->id)->first();
+        if ($courses) {
+            foreach ($courses as $course) {
+                $course->department_id = $_POST['department_id'];
+            }
+            $department->delete();
+            return Redirect::route('departments.index')->with('status', 'Deleted Successfully, but there were courses in this department.');
+        } else {
+            $department->delete();
+            return Redirect::route('departments.index')->with('status', 'Deleted Successfully.');
+        }
+    }
 
-       if($check!=""||$check2!=""){
-        return Redirect::route('departments.index')->with('warning','Warning CanNot Deleted Delete Child First.');
-
-       }
-       else{
-        $department ->delete();
-        return Redirect::route('departments.index')->with('status','Deleted Sucessfuly .');
-       }
+    public function resotore($id)
+    {
+        $department = Department::onlyTrashed()->where('id', $id)->first();
+        $department->restore();
+        return Redirect::route('departments.index')->with('status', 'Restored Successfully.');
     }
 }
