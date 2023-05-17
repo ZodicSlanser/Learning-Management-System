315b033946e60f943b3db32a26ea27fb02dc0cea
ZodicSlanser <zodic.slanser68@yahoo.com>
2023-05-17 04:23:59 +0300

    fixed delete function

--- app/Models/Enrollment.php
+++ app/Models/Enrollment.php
@@ -2,10 +2,9 @@
 
 namespace App\Models;
 
-use App\Models\Course;
-use Illuminate\Database\Eloquent\Model;
 use Database\Factories\EnrollmentFactory;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 
 class Enrollment extends Model
 {
@@ -29,7 +28,12 @@ class Enrollment extends Model
 
     public function course()
     {
-        return $this->belongsTo(Course::class,'course_id');
+        return $this->belongsTo(Course::class, 'course_id');
+    }
+    public function delete(){
+        $this->student()->dissociate();
+        $this->course()->dissociate();
+        parent::delete();
     }
 
 }
