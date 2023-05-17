9e2660b885b6c51acd291d9c0b027754d0e2df0f
ZodicSlanser <zodic.slanser68@yahoo.com>
2023-05-17 04:24:23 +0300

    implemented the respective functionality

--- resources/views/departments/index.blade.php
+++ resources/views/departments/index.blade.php
@@ -8,51 +8,52 @@
     <title>Document</title>
 </head>
 <body>
-    @extends('extend')
-    @section('content')
-    
+@extends('extend')
+@section('content')
+
     @if (session('status'))
-    <div class="alert alert-success">
-          {{ session('status') }}
-   </div>
-   @endif
-   @if (session('warning'))
-   <div class="alert alert-danger">
-         {{ session('warning') }}
-  </div>
-  @endif
-   <h1>
+        <div class="alert alert-success">
+            {{ session('status') }}
+        </div>
+    @endif
+    @if (session('warning'))
+        <div class="alert alert-danger">
+            {{ session('warning') }}
+        </div>
+    @endif
+    <h1>
         <a href="{{route('departments.create')}}"> ADD NEW DEPARTMENTS
-            
+
         </a>
     </h1>
-<br><br>
-     @foreach ($departments as $department)
-       <div class="d-flex justify-content-between">
+    <br><br>
+    @foreach ($departments as $department)
+        <div class="d-flex justify-content-between">
             <div>
-            <a href= "{{route('departments.show',$department->id)}}">
-                {{$department->id}}-{{$department->name}} 
+                <a href="{{route('departments.show',$department->id)}}">
+                    {{$department->id}}-{{$department->name}}
 
-            </a>
+                </a>
             </div>
 
-         <div>
-            <a href="{{route('departments.edit',$department->id)}}" class="btn btn-info" style="margin-right: 150px;margin-top: 10px;"> EDIT </a>
+            <div>
+                <a href="{{route('departments.edit',$department->id)}}" class="btn btn-info"
+                   style="margin-right: 150px;margin-top: 10px;"> EDIT </a>
 
-            <form action="{{route('departments.destroy',$department->id)}}" method="POST">
-                @csrf
-                @method('DELETE')
-                <button class="btn btn-danger" style="margin-left: 120px;margin-top: -69px;">DELETE</button>
-            </form>
-        </div>
+                <form action="{{route('departments.destroy',$department->id)}}" method="POST">
+                    @csrf
+                    @method('DELETE')
+                    <button class="btn btn-danger" style="margin-left: 120px;margin-top: -69px;">DELETE</button>
+                </form>
+            </div>
         </div>
     @endforeach
-   <a href="{{route('courses.index')}}" class="btn btn-primary">Courses</a>
-   <a href="{{route('generate.index')}}" class="btn btn-primary">Generate</a>
-   <a href="{{route('users.index')}}" class="btn btn-primary">Users</a>
-   {{ $departments->links() }}
-    @endsection
-    
+    <a href="{{route('courses.index')}}" class="btn btn-primary">Courses</a>
+    <a href="{{route('generate.index')}}" class="btn btn-primary">Generate</a>
+    <a href="{{route('users.index')}}" class="btn btn-primary">Users</a>
+    {{ $departments->links() }}
+@endsection
+
 
 </body>
 </html>
