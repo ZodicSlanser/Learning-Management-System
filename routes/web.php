3151bd3c4aa2e80d38ad31590fe03ebe21d14f2a
ZodicSlanser <zodic.slanser68@yahoo.com>
2023-05-17 04:16:05 +0300

    defined more routes for department, user and enrollment

--- routes/web.php
+++ routes/web.php
@@ -1,29 +1,28 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\UsersController;
 use App\Http\Controllers\CoursesController;
-use App\Http\Controllers\GenerateController;
 use App\Http\Controllers\DepartmentsController;
+use App\Http\Controllers\GenerateController;
+use App\Http\Controllers\UsersController;
+use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::resource('/courses',CoursesController::class);
-Route::resource('/departments',DepartmentsController::class);
-Route::resource('/users',UsersController::class);
-Route::resource('/generate',GenerateController::class);
+Route::resource('/courses', CoursesController::class);
+Route::resource('/departments', DepartmentsController::class);
+Route::resource('/users', UsersController::class);
+Route::resource('/generate', GenerateController::class);
+
+Route::get('department-restore/{id}', [DepartmentsController::class, 'delete'])->name('department.delete');
+
+Route::get('course-restore/{id}', [CoursesController::class, 'delete'])->name('course.delete');
+
+Route::get('user/restore/{id}', [UsersController::class, 'restore'])->name('user.restore');
+
+Route::get('delete-enrollment', [GenerateController::class, 'deleteEnrollment'])->name('enrollment.delete');
+Route::get('/generate/download/{course_id}', [GenerateController::class, 'download'])->name('generate.download');
 
 
