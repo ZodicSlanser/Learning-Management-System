8cc53d01b5ba6aea3f5316118964474b5b0027f3
ZodicSlanser <zodic.slanser68@yahoo.com>
2023-05-18 20:53:23 +0300

    formatted code for better readability

--- resources/views/users/edit.blade.php
+++ resources/views/users/edit.blade.php
@@ -79,10 +79,11 @@
         </div>
         <div>
             <br>
-            <span><input name="role" type="radio" value="{{$users->role}}" checked> @if($users->role=="2"){{"Doctors"}}
-            @else 
-            {{"Students"}}
-            @endif
+            <span><input name="role" type="radio" value="{{$users->role}}" checked> @if($users->role=="2")
+                    {{"Doctors"}}
+                @else
+                    {{"Students"}}
+                @endif
             </span>
 
             @error('role')
