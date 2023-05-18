e2c1b77fbb7147d95b1a7c9eff3574123d0c04bb
ZodicSlanser <zodic.slanser68@yahoo.com>
2023-05-18 20:54:36 +0300

    added routes for all new placeholders and their functions

--- routes/web.php
+++ routes/web.php
@@ -16,13 +16,16 @@ Route::resource('/departments', DepartmentsController::class);
 Route::resource('/users', UsersController::class);
 Route::resource('/generate', GenerateController::class);
 
-Route::get('department-restore/{id}', [DepartmentsController::class, 'delete'])->name('department.delete');
+Route::get('/departments-restore', [DepartmentsController::class, 'restore_index'])->name('departments.restore.index');
+Route::get('/departments/restore/do', [DepartmentsController::class, 'restore'])->name('department.restore');
 
-Route::get('course-restore/{id}', [CoursesController::class, 'delete'])->name('course.delete');
+Route::get('/courses-restore', [CoursesController::class, 'restore_index'])->name('course.restore.index');
+Route::get('/courses/restore/do', [CoursesController::class, 'restore'])->name('course.restore');
 
-Route::get('user/restore/{id}', [UsersController::class, 'restore'])->name('user.restore');
+Route::get('/users-restore', [UsersController::class, 'restore_index'])->name('user.restore.index');
+Route::get('/users-restore/do', [UsersController::class, 'restore'])->name('user.restore');
 
-Route::get('delete-enrollment', [GenerateController::class, 'deleteEnrollment'])->name('enrollment.delete');
+Route::get('/delete-enrollment', [GenerateController::class, 'deleteEnrollment'])->name('enrollment.delete');
 Route::get('/generate/download/{course_id}', [GenerateController::class, 'download'])->name('generate.download');
 
 
