ae06288a43b22b8933b78516f98a3390b69a365d
ZodicSlanser <zodic.slanser68@yahoo.com>
2023-05-23 21:54:44 +0300

    merged UserController.php with UsersController.php

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -3,8 +3,10 @@
 namespace App\Http\Controllers;
 
 use App\Models\Department;
+use App\Models\Role;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Redirect;
 use Illuminate\Support\Str;
@@ -163,5 +165,47 @@ class UsersController extends Controller
         $id = Request()->id;
         User::onlyTrashed()->where('id', $id)->first()->restore();
         return Redirect::route('user.restore.index')->with('status', 'Restored Successfully.');
+    }
+
+    function loginindex()
+    {
+        return view('login');
+    }
+
+    function checklogin(Request $request)
+    {
+        $this->validate($request, [
+            'email' => 'required|email',
+            'password' => 'required|alphaNum|min:3'
+        ]);
+
+        $user_data = array(
+            'email' => $request->get('email'),
+            'password' => $request->get('password')
+        );
+
+        if (!(Auth::attempt($user_data))) {
+            return back()->with('error', 'Wrong Login Details');
+        }
+        return redirect('home');
+    }
+
+    function successlogin()
+    {
+        if (Auth::user()->role == Role::ADMIN) {
+            return view('admin_homepage');
+        }
+        if (Auth::user()->role == Role::PROFESSOR) {
+            return view('professor_homepage');
+        }
+        if (Auth::user()->role == Role::STUDENT) {
+            return view('student_homepage');
+        }
+    }
+
+    function logout()
+    {
+        Auth::logout();
+        return redirect('login');
     }
 }
