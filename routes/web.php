8cde2c1574b7cd2c7e854fc9fcc539863335ff14
ZodicSlanser <zodic.slanser68@yahoo.com>
2023-05-23 21:55:12 +0300

    basic implementations for the middleware

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Middleware\CheckAdminRole;
+use App\Http\Middleware\CheckProfessorRole;
+use App\Models\Role;
 use App\Models\User;
 use App\Models\Course;
 use App\Models\Enrollment;
@@ -19,29 +22,24 @@ Route::get('/', function () {
     return view('login');
 });
 
-Route::get('/login', [UserController::class, 'index']);
-Route::post('/login/checklogin', [UserController::class, 'checklogin']);
-Route::get('/home', [UserController::class, 'successlogin']);
-Route::get('/logout', [UserController::class, 'logout']);
+Route::get('/login', [UsersController::class, 'loginIndex']);
+Route::post('/login/checklogin', [UsersController::class, 'checklogin']);
+Route::get('/home', [UsersController::class, 'successlogin']);
+Route::get('/logout', [UsersController::class, 'logout']);
 
 Route::get('/professorCourses', [ProfessorsController::class, 'getCoursesByProfessorId']);
 
 Route::get('/students' , function (){
 
-    $students = User::where('role', '=' , 3)->get();
+    $students = User::where('role', '=' , Role::STUDENT)->get();
 
    return view('student_list' , ['students'=> $students]);  //pass anything inside ass_array to view as variable
 });
 
 Route::get('/students/{id}', function ($id) {
     $item = Enrollment::where('student_id'  , $id)->with('course')->get();
-
-
     return view('student_info', [ 'student' => $item ]);
  })->name('/students/{id}.x');
-
-
-
 
  Route::get('/regcourse/{id}', function ($id) {
     $item = User::find($id);
@@ -51,42 +49,39 @@ Route::get('/students/{id}', function ($id) {
  });
 
 
-
  Route::post('/supcourse', function (Request $requset) {
 
-   $formfields = $requset->validate([
-    'course_id' => 'required',
-    'student_id' => 'required'
-   ]);
+     $formfields = $requset->validate([
+         'course_id' => 'required',
+         'student_id' => 'required'
+     ]);
 
-
-   $course = Course::find($formfields['course_id']);
-   $student = User::find($formfields['student_id']);
-
-   $x = new Enrollment();
-   $enroll = $x->can_enroll($student, $course);
-
-   if ($enroll) {
-      Enrollment::create($formfields);
-      return 'Added';
-   }  else{
-
-   return 'Nooooo';
-   }
-
+     $course = Course::find($formfields['course_id']);
+     $student = User::find($formfields['student_id']);
+     $enrollemt = new Enrollment();
+     return $enrollemt->enroll($student,$course);
    // return Redirect::route('/students');
  });
 
-Route::resource('/courses', CoursesController::class);
-Route::resource('/departments', DepartmentsController::class);
-Route::resource('/users', UsersController::class);
-Route::resource('/generate', GenerateController::class);
-Route::get('/departments-restore', [DepartmentsController::class, 'restore_index'])->name('departments.restore.index');
-Route::get('/departments/restore/do', [DepartmentsController::class, 'restore'])->name('department.restore');
-Route::get('/courses-restore', [CoursesController::class, 'restore_index'])->name('course.restore.index');
-Route::get('/courses/restore/do', [CoursesController::class, 'restore'])->name('course.restore');
-Route::get('/users-restore', [UsersController::class, 'restore_index'])->name('user.restore.index');
-Route::get('/users-restore/do', [UsersController::class, 'restore'])->name('user.restore');
-Route::get('/delete-enrollment', [GenerateController::class, 'deleteEnrollment'])->name('enrollment.delete');
-Route::get('/generate/download/{course_id}', [GenerateController::class, 'download'])->name('generate.download');
+Route::middleware(CheckProfessorRole::class)->group(function () {
+    //professor routes
+});
+
+Route::middleware(CheckAdminRole::class)->group(function () {
+    //admin routes
+    Route::get('/generate/download/{course_id}', [GenerateController::class, 'download'])->name('generate.download');
+    Route::get('/departments-restore', [DepartmentsController::class, 'restore_index'])->name('departments.restore.index');
+    Route::get('/departments/restore/do', [DepartmentsController::class, 'restore'])->name('department.restore');
+    Route::get('/courses-restore', [CoursesController::class, 'restore_index'])->name('course.restore.index');
+    Route::get('/courses/restore/do', [CoursesController::class, 'restore'])->name('course.restore');
+    Route::get('/users-restore', [UsersController::class, 'restore_index'])->name('user.restore.index');
+    Route::get('/users-restore/do', [UsersController::class, 'restore'])->name('user.restore');
+    Route::get('/delete-enrollment', [GenerateController::class, 'deleteEnrollment'])->name('enrollment.delete');
+    Route::resource('/courses', CoursesController::class);
+    Route::resource('/departments', DepartmentsController::class);
+    Route::resource('/users', UsersController::class);
+    Route::resource('/generate', GenerateController::class);
+});
+
+
 
