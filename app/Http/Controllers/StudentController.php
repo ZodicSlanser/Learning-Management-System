c384e9843b899ce2c1d3e6b3dfebc57195c50561
ZodicSlanser <zodic.slanser68@yahoo.com>
2023-05-25 00:00:26 +0300

    updated the validation before showing courses

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -2,21 +2,26 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Role;
-use App\Models\User;
 use App\Models\Course;
 use App\Models\Enrollment;
+use App\Models\User;
 use Illuminate\Http\Request;
-use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Facades\Redirect;
+use Illuminate\Support\Facades\Storage;
 
 class StudentController extends Controller
 {
-    public function index($id)
+    public function index(Request $request)
     {
-        $course = Course::find($id);
+        $student = Auth::user();
+        $courseId = $request->id;
+        $course = Course::findOrFail($courseId);
+
+        if (!$student->has_enrolled_in($course)) {
+            abort(403, 'Unauthorized');
+        }
+
         $path_subject = "public/material/" . $course->name;
 
         // Retrieve the files uploaded by the uploadFiles method
@@ -40,46 +45,45 @@ class StudentController extends Controller
     }
 
 
+    public function show_students()
+    {
+        $students = User::where('role', '=', 3)->get();
 
-
-
-
-    public function show_students(){
-        $students = User::where('role', '=' , 3)->get();
-
-        return view('student_list' , ['students'=> $students]);  //pass anything inside ass_array to view as variable
+        return view('student_list', ['students' => $students]);  //pass anything inside ass_array to view as variable
 
     }
 
-    public function course_ofstudent(){
+    public function course_ofstudent()
+    {
         $id = Auth::user()->id;
-        $item = Enrollment::where('student_id'  , $id)->with('course')->get();
-        return view('student_info', [ 'student' => $item ]);
+        $item = Enrollment::where('student_id', $id)->with('course')->get();
+        return view('student_info', ['student' => $item]);
 
     }
 
-    public function registration(){
+    public function registration()
+    {
         $user = Auth::user();
-        $unenrolled_courses = Enrollment::where('student_id' ,"!=",$user->id)->get();
-        return view('reg_course' , [ 'courses' => $unenrolled_courses, 'item' => $user, ]);
+        $unenrolled_courses = Enrollment::where('student_id', "!=", $user->id)->get();
+        return view('reg_course', ['courses' => $unenrolled_courses, 'item' => $user,]);
     }
 
-    public function reg_form(Request $requset){
+    public function reg_form(Request $requset)
+    {
         $formfields = $requset->validate([
             'course_id' => 'required'
-           ]);
+        ]);
 
-           $formfields['student_id'] = Auth::user()->id;
-           $course = Course::find($formfields['course_id']);
-           $student = User::find($formfields['student_id']);
-           $x = new Enrollment();
-           $enroll = $x->enroll($student, $course);
-           if ($enroll) {
-              Enrollment::create($formfields);
-              return Redirect::route('students.x');
-           }
-           return \redirect()->back()->with('status', 'You are already enrolled in this course');
-
+        $formfields['student_id'] = Auth::user()->id;
+        $course = Course::find($formfields['course_id']);
+        $student = User::find($formfields['student_id']);
+        $x = new Enrollment();
+        $enroll = $x->enroll($student, $course);
+        if ($enroll) {
+            Enrollment::create($formfields);
+            return Redirect::route('students.x');
+        }
+        return \redirect()->back()->with('status', 'You are already enrolled in this course');
 
 
     }
